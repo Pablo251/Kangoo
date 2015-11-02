5316dcd0ed7692ee31af670c5e0d39b7527ae875
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-11-02 16:14:29 -0600

    cache

--- app/cache/c__xampp_htdocs_kangoo_app_views_index.volt.php
+++ app/cache/c__xampp_htdocs_kangoo_app_views_index.volt.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <?php echo $this->tag->stylesheetLink('css/materialize.min.css'); ?>
+        <!-- <link href="http://materializecss.com/templates/parallax-template/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/> -->
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Phalcon PHP Framework</title>
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="Kangoo as a WebMail manager">
+        <meta name="author" content="Susana Corrales & Pablo Arce Cascante">
+        <?php echo $this->tag->getTitle(); ?>
     </head>
     <body>
         <?php echo $this->getContent(); ?>
+        <?php echo $this->tag->javascriptInclude('js/jquery-2.1.4.min.js'); ?>
+        <?php echo $this->tag->javascriptInclude('js/materialize.min.js'); ?>
     </body>
 </html>
--- app/cache/c__xampp_htdocs_kangoo_app_views_index_index.volt.php
+++ app/cache/c__xampp_htdocs_kangoo_app_views_index_index.volt.php
@@ -1,7 +1,16 @@
-<div class="page-header">
-    <h1>Congratulations!</h1>
+<div id="index-banner" class="parallax-container">
+  <div class="section no-pad-bot">
+    <div class="container">
+      <br><br>
+      <h1 class="header center teal-text text-lighten-2">Kangoo WebMail Manager</h1>
+      <div class="row center">
+        <h5 class="header col s12 light">Send mails and manage them with Kangoo... Lets Jump!</h5>
+      </div>
+      <div class="row center">
+        <?php echo $this->tag->linkTo(array('session/signup', 'Sign up', 'class' => 'btn-large waves-effect waves-light teal lighten-1')); ?>
+        
+      </div>
+      <br><br>
+    </div>
+  </div>
 </div>
-
-<p>You're now flying with Phalcon. Great things are about to happen!</p>
-
-<em>This page is located at views/index/index.phtml</em>
