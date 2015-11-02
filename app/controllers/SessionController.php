7cbb9d601daec9c94af788508d8cc6d4bc02f46a
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-11-02 12:32:22 -0600

    Session: csrf enabled, controller improved
    Some logic added in the controller,
    csrf enabled in the form and view

--- app/controllers/SessionController.php
+++ app/controllers/SessionController.php
@@ -2,24 +2,44 @@
 use Phalcon\Mvc\Controller;
 class SessionController extends ControllerBase
 {
-    /*Initialize This Controller*/
-    public function initialize(){
-      $this->tag->setTitle("Sign up");
-      parent::initialize();
-    }
+  /*Initialize This Controller*/
+  public function initialize(){
+    $this->tag->setTitle("Sign up");
+    parent::initialize();
+  }
 
-    public function indexAction()
-    {
-    }
-    /**
-    * execute the signup ation, face to an existent user.
-    */
-    public function signupAction()
-    {
-      $myForm = new SignUpForm();
-      if ($this->request->isPost()) {
-        echo "Esto es un post";
+  public function indexAction()
+  {
+  }
+  /**
+  * execute the signup ation, face to an existent user.
+  */
+  public function signupAction()
+  {
+    $myForm = new SignUpForm();
+    if ($this->request->isPost()) {
+
+      if ($form->isValid($this->request->getPost()) != false) {
+
+        $user = new Users();
+
+        $user->assign(array(
+          'name' => $this->request->getPost('name', 'striptags'),
+          'email' => $this->request->getPost('email'),
+          'password' => $this->security->hash($this->request->getPost('password')),
+          'profilesId' => 2
+        ));
+
+        if ($user->save()) {
+          return $this->dispatcher->forward(array(
+            'controller' => 'index',
+            'action' => 'index'
+          ));
+        }
+
+        $this->flash->error($user->getMessages());
       }
-      $this->view->form = $myForm;
     }
+    $this->view->form = $myForm;
+  }
 }
