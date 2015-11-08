3fbc7a1510a7b3920ea37ede20fc60c5c43cf024
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-11-07 20:43:23 -0600

    Mail confirmation ready
    + Users can activate they accounts through confirmation link
    - Users can't login, login is not build yet

--- app/controllers/SessionController.php
+++ app/controllers/SessionController.php
@@ -37,4 +37,57 @@ class SessionController extends ControllerBase
     }
     $this->view->form = $form;
   }
+  /**
+   * Account confirmation through the previous sent mail.
+   */
+  public function confirmEmailAction()
+  {
+      $code = $this->dispatcher->getParam('code');
+
+      $confirmation = EmailConfirmations::findFirstByCode($code);
+
+      if (!$confirmation) {
+          return $this->dispatcher->forward(array(
+              'controller' => 'index',
+              'action' => 'index'
+          ));
+      }
+      if ($confirmation->confirmed != 'N') {
+          return $this->dispatcher->forward(array(
+              'controller' => 'session',
+              'action' => 'login'
+          ));
+      }
+
+      $confirmation->confirmed = 'Y';
+
+      $confirmation->user->active = 1;
+
+      /**
+       * Change the confirmation to 'confirmed' and update the user to 'active'
+       */
+      if (!$confirmation->save()) {
+
+          foreach ($confirmation->getMessages() as $message) {
+              $this->flash->error($message);
+          }
+
+          return $this->dispatcher->forward(array(
+              'controller' => 'index',
+              'action' => 'index'
+          ));
+      }
+
+      /**
+       * Identify the user in the application
+       */
+      //$this->auth->authUserById($confirmation->user->id_user);
+
+      $this->flash->success('The email was successfully confirmed');
+
+      return $this->dispatcher->forward(array(
+          'controller' => 'index',
+          'action' => 'index'
+      ));
+  }
 }
