bb6bf524368cb490ed0da857cda1bc2946533eb7
Pablo Arce Cascante <josepa_arce@hotmail.com>
2015-12-06 06:41:49 -0600

    Inprovement...
    +more order in the js code
    +action next validate

--- public/js/dash.js
+++ public/js/dash.js
@@ -3,36 +3,20 @@
 * Manage actions like fill a table with a new stack with mails through Ajax Post
 */
 $(document).ready(function(){
-  /*Ajax GET: count of the mails in the stack*/
-  $.ajax({
-    url: "/kangoo/dashboard/getMailStack"
-  }).done(function(data)
-  {
-    /*Set the data into the localStorage*/
-    localStorage.setItem("localStack",data);
-  });
-  /*Ajax post: Sent the mailStack number and response with new mails stack*/
-    $.ajax({
-    data:  {"sentstack" : localStorage.localStack},
-    url:   '/kangoo/dashboard/outputCharger',
-    type:  'post',
-    beforeSend: function () {
-      console.log("Processing post mails count and get new stack");
-    },
-    success:  function (response) {
-      KANGOO.appendSentRows(response);
-    }
-  });
+  /*Init*/
+  KANGOO.initDash();
   /*Click event for sent mails button*/
   $("#exsent").click(function() {
     KANGOO.outputAjaxPOST(2, 6);
   });
   /*Click event for next button*/
   $("#next").click(function() {
-    KANGOO.callStack(localStorage.finalLoop);
+    if (parseInt(localStorage.finalLoop)>0) {
+      KANGOO.callStack(localStorage.finalLoop);
+    }
   });
   /*Click event for the back button*/
-  $("#back").click(function() {parseInt(localStorage.initialLoop)
+  $("#back").click(function() {
     var sum = parseInt(localStorage.initialLoop)+parseInt(localStorage.diference);
     console.log(sum);
     if (sum>parseInt(localStorage.localStack)) {
